4bc90596ada4636a8b64d0851f58ebc983d9b497
David Rodriguez <davisito544@gmail.com>
2021-01-10 01:05:07 -0500

    se creo el controlador para el registro y inicio de sesion del usuario

---
+++ backEnd/controller/usuario.js
@@ -0,0 +1,41 @@
+const { Usuario } = require("../model/usuario");
+const passport = require("passport");
+const error_types = require("./error_types");
+
+//controlador para el registro del usuario
+const registro = (req, res, next) => {
+	Usuario.findOne({ nombreUsuario: req.body.nombreUsuario })
+		.then((data) => {
+			if (data) {
+				throw new error_types.InfoError("Este nombre de usuario ya existe");
+			} else {
+				const usuario = new Usuario();
+				usuario.nombre = req.body.nombre;
+				usuario.nombreUsuario = req.body.nombreUsuario;
+				usuario.setPassword(req.body.clave);
+				usuario.localidad = req.body.localidad;
+				return usuario.save();
+			}
+		})
+		.then((data) => {
+			res.status(200).send("Registro Exitoso")
+		})
+		.catch((err) => {
+			next(err);
+		});
+};
+//controlador para el inicio de sesion del usuario
+const login = (req, res, next) => {
+	passport.authenticate("local", { session: false }, (error, user) => {		
+		if (error || !user) {
+			next(new error_types.Error404("Usuario o Contraseña incorrectos"));
+		} else {
+			const token = user.generateJWT();
+			res.status(200).json({ token });
+		}
+	})(req, res);
+};
+module.exports = {
+	registro,
+	login,
+};
