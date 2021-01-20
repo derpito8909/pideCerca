4136061ae9f5015b7dd9f12ac87af18617bee41a
David Rodriguez <davisito544@gmail.com>
2021-01-19 23:39:58 -0500

    se creo la ruta para los compomentes de login y registro

--- frontEnd/bogotaFootballLeague/src/app/app-routing.module.ts
+++ frontEnd/bogotaFootballLeague/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./principal/login/login.component";
+import { RegistroComponent } from "./principal/registro/registro.component";
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [  
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'register', component: RegistroComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
